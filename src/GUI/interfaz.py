f6814892dd09148d2c5b614cd63fe1316738bfd0
msanavarro <msarmientonavarro@gmail.com>
2020-01-03 18:16:20 -0600

    Contribución inicial de código y documentacón

---
+++ src/GUI/interfaz.py
@@ -0,0 +1,9 @@
+'''
+Created on 18 dic. 2019
+
+@author: msanavarro
+'''
+
+import tkinter
+from nucleoDA import *
+
---
+++ src/nucleo/funcionesAD.py
@@ -0,0 +1,33 @@
+'''
+Created on 18 dic. 2019
+
+@author: msanavarro
+'''
+
+#===============================================================================
+# from pyad import *
+#===============================================================================
+import logging
+
+def conectar(ldap, usuario, contrasena):
+    exito = False
+    return exito
+
+def alta(conexion, nombre, contrasena):
+    exito = False
+    return exito
+
+def cambiaContrasena():
+    exito = False
+    return exito
+
+logging.basicConfig(filename='/Users/msanavarro/workspace/isat.ad/logs/isatAD.log', 
+                    format='%(asctime)s : %(levelname)s : %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', 
+                    level=logging.DEBUG)
+logging.info('Hola')
+#===============================================================================
+# pyad.set_defaults(ldap_server='churintzio.mx', username='Administrador', password='Julia0544')
+#===============================================================================
+
+
